8db3d0090d6e706e3980cc7cc32ab42bee629d5a
Muhammad27775 <muhammad.moazzam2775@gmail.com>
2026-06-28 00:26:47 +0500

    Added The Sates.py File

---
+++ src/states.py
@@ -0,0 +1,12 @@
+from enum import Enum, auto
+
+class GameState(Enum):
+    """Tracks the top-level screens and gameplay modes."""
+    MAIN_MENU = auto()
+    NAME_INPUT = auto()
+    MULTIPLAYER_MENU = auto()
+    PLAYING = auto()
+    PAUSED = auto()
+    OPTIONS = auto()
+    BACKGROUND_MENU = auto()
+    GAME_OVER = auto()
